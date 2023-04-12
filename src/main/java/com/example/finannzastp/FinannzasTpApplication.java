df404510eb2753271784a8a421840845ed319229
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2023-04-12 12:19:21 -0500

    first commit

---
+++ src/main/java/com/example/finannzastp/FinannzasTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.finannzastp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinannzasTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinannzasTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finannzastp/FinannzasTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finannzastp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinannzasTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
